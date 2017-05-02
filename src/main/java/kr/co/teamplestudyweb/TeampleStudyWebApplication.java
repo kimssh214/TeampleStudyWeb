e1085e44ad6b764eba2827d8e055db8e64f594b0
unknown <>
2017-05-02 13:46:39 +0900

    Start Project

---
+++ src/main/java/kr/co/teamplestudyweb/TeampleStudyWebApplication.java
@@ -0,0 +1,12 @@
+package kr.co.teamplestudyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeampleStudyWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeampleStudyWebApplication.class, args);
+	}
+}
